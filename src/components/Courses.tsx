3d7197c94145d1f9d26e2cdeda72b8bc7e0022e3
linjiang82 <linjiang82@gmail.com>
2020-03-19 21:30:09 +1000

    changed to FC and useEffect from class component

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,35 +1,92 @@
-import React from 'react'
-import Course from './Course'
-import {connect} from 'react-redux'
-import {TCourse, TRootReducer} from '../types/types'
-import { db } from '../firebase/config'
-import { fetchCourses } from '../actions/action'
-import store from '../store';
-import './Courses.css'
+import React, { useRef, useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
+import Course from "./Course";
+import { connect } from "react-redux";
+import { TCourse, TRootReducer } from "../types/types";
+import { db } from "../firebase/config";
+import { fetchCourses } from "../actions/action";
+import store from "../store";
+import styles from "./Courses.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import StarRate from "./StarRate";
 
- interface myProps {
-  courses:TCourse[];
+interface myProps {
+  courses: TCourse[];
 }
-class Courses extends React.Component<myProps>{
-  componentDidMount(){
-    db.collection('Courses').get().then((snapshot)=>snapshot.forEach(doc=>store.dispatch(fetchCourses({...doc.data(),id:doc.id})))).catch(e=>console.log("error"+e));
-    // (async ()=> { const data = await db.collection('Courses').get(); 
-    //             const items = await data.forEach(doc=>{console.log(doc.data())})})()
-  }
-  render(){
-    let courses:JSX.Element[]=[];
-    this.props.courses.map(course=>courses.push(<Course key={course.id} data={course}></Course>))
-   if(this.props.courses.length>0) 
-   console.log(this.props.courses.length) 
-    return <div className='flexCtn'>
-      { courses }
+
+const Courses: React.FC<{ courses: TCourse[] }> = props => {
+  const popOver = useRef<HTMLDivElement>(null);
+  const [courseState, setCourse] = useState<undefined | TCourse>(undefined);
+  const [quicklookId, setQuicklookId] = useState<null | string>(null);
+  const handleQuicklook = (e: React.MouseEvent<HTMLDivElement>): void => {
+    setQuicklookId(e.currentTarget.id);
+    // if(popOver.current)
+    // popOver.current.classList.add(styles.showPopover)
+  };
+  const closeQuicklook = () => {
+    setCourse(undefined);
+    setQuicklookId(null); //add this so useEffect will run again, otherwise due to quicklookId doesnot change, so the below useeffect
+  };
+  useEffect(() => {
+    console.log(quicklookId);
+    setCourse(props.courses.find(i => i.id === quicklookId));
+  }, [props.courses, quicklookId]);
+  let courses: JSX.Element[] = [];
+  props.courses.map(course => {
+    courses.push(
+      <Course
+        key={course.id}
+        handleQuicklook={handleQuicklook}
+        data={course}></Course>
+    );
+  });
+  useEffect(() => {
+    console.log("render");
+    if (props.courses.length === 0)
+      db.collection("Courses")
+        .get()
+        .then(snapshot =>
+          snapshot.forEach(doc =>
+            store.dispatch(fetchCourses({ ...doc.data(), id: doc.id }))
+          )
+        )
+        .catch(e => console.log("error" + e));
+  }, [props.courses.length]);
+  return (
+    <div className={styles.courses}>
+      {courseState !== undefined && (
+        <div ref={popOver} className={styles.popover}>
+          <div className={styles.quicklook}>
+            <div className={styles.lhs}>
+              <img
+                className={styles.avatar}
+                src={courseState.thumbURL}
+                alt='avatar'></img>
+            </div>
+            <div className={styles.rhs}>
+              <div className={styles.author}>{courseState.author}</div>
+              <div className={styles.name}>{courseState.name}</div>
+              <div className={styles.rate}>
+                <StarRate rate={courseState.rate}></StarRate>
+              </div>
+              <div className={styles.price}>${courseState.price}</div>
+              <Link to={`detail/${quicklookId}`}><button className={styles.btn}>See Product Details</button></Link>
+            </div>
+            <FontAwesomeIcon
+              icon={faWindowClose}
+              onClick={closeQuicklook}
+              className={styles.icon}></FontAwesomeIcon>
+          </div>
+        </div>
+      )}
+      <div className={styles.flexCtn}>{courses}</div>
     </div>
-  }
-}
+  );
+};
 
-const mapStateToProps = (state: TRootReducer ):myProps => {
-  return { courses: state.courses,}
-  
-}
+const mapStateToProps = (state: TRootReducer): myProps => {
+  return { courses: state.courses };
+};
 
-export default connect(mapStateToProps)(Courses)
+export default connect(mapStateToProps)(Courses);
