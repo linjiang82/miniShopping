5bc5419e0379d47987732f99bd2f8a8314b00948
linjiang82 <linjiang82@gmail.com>
2020-04-16 21:10:26 +1000

    add function of filter products by category

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,8 +1,12 @@
+/*
+1.used useSelector instead of connect for React FC.courses
+*/
+
 import React, { useRef, useEffect, useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Course from "./Course";
-import { connect } from "react-redux";
-import { TCourse, TRootReducer } from "../types/types";
+import { useSelector } from "react-redux";
+import { TCourse, TSearch, TRootReducer } from "../types/types";
 import { db } from "../firebase/config";
 import { fetchCourses } from "../actions/action";
 import store from "../store";
@@ -11,12 +15,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 import StarRate from "./StarRate";
 
-interface myProps {
-  courses: TCourse[];
-}
+// interface myProps {
+//   courses: TCourse[];
+//   search: TSearch;
+// }
 
-const Courses: React.FC<{ courses: TCourse[] }> = props => {
+const Courses: React.FC = () => {
   const popOver = useRef<HTMLDivElement>(null);
+  const coursesState: TCourse[] = useSelector(
+    (state: TRootReducer) => state.courses
+  );
+  const searchState: TSearch = useSelector(
+    (state: TRootReducer) => state.search
+  );
+  console.log(searchState);
   const [courseState, setCourse] = useState<undefined | TCourse>(undefined);
   const [quicklookId, setQuicklookId] = useState<null | string>(null);
   const handleQuicklook = (e: React.MouseEvent<HTMLDivElement>): void => {
@@ -28,31 +40,37 @@ const Courses: React.FC<{ courses: TCourse[] }> = props => {
     setCourse(undefined);
     setQuicklookId(null); //add this so useEffect will run again, otherwise due to quicklookId doesnot change, so the below useeffect
   };
-  useEffect(() => {
-    console.log(quicklookId);
-    setCourse(props.courses.find(i => i.id === quicklookId));
-  }, [props.courses, quicklookId]);
   let courses: JSX.Element[] = [];
-  props.courses.map(course => {
-    courses.push(
-      <Course
-        key={course.id}
-        handleQuicklook={handleQuicklook}
-        data={course}></Course>
-    );
+  coursesState.map((course) => {
+    console.log(searchState.catId);
+    if (searchState.catId === null)
+      courses.push(
+        <Course
+          key={course.id}
+          handleQuicklook={handleQuicklook}
+          data={course}></Course>
+      );
+    else if (course.category.id === searchState.catId)
+      courses.push(
+        <Course
+          key={course.id}
+          handleQuicklook={handleQuicklook}
+          data={course}></Course>
+      );
   });
   useEffect(() => {
-    console.log("render");
-    if (props.courses.length === 0)
-      db.collection("Courses")
-        .get()
-        .then(snapshot =>
-          snapshot.forEach(doc =>
-            store.dispatch(fetchCourses({ ...doc.data(), id: doc.id }))
-          )
+    setCourse(coursesState.find((i) => i.id === quicklookId));
+  }, [coursesState, quicklookId]);
+  useEffect(() => {
+    db.collection("Courses")
+      .get()
+      .then((snapshot) =>
+        snapshot.forEach((doc) =>
+          store.dispatch(fetchCourses({ ...doc.data(), id: doc.id }))
         )
-        .catch(e => console.log("error" + e));
-  }, [props.courses.length]);
+      )
+      .catch((e) => console.log("error" + e));
+  }, []);
   return (
     <div className={styles.courses}>
       {courseState !== undefined && (
@@ -71,7 +89,9 @@ const Courses: React.FC<{ courses: TCourse[] }> = props => {
                 <StarRate rate={courseState.rate}></StarRate>
               </div>
               <div className={styles.price}>${courseState.price}</div>
-              <Link to={`detail/${quicklookId}`}><button className={styles.btn}>See Product Details</button></Link>
+              <Link to={`detail/${quicklookId}`}>
+                <button className={styles.btn}>See Product Details</button>
+              </Link>
             </div>
             <FontAwesomeIcon
               icon={faWindowClose}
@@ -85,8 +105,12 @@ const Courses: React.FC<{ courses: TCourse[] }> = props => {
   );
 };
 
-const mapStateToProps = (state: TRootReducer): myProps => {
-  return { courses: state.courses };
-};
+// const mapStateToProps = (state: TRootReducer): myProps => {
+//   return {
+//     courses: state.courses,
+//     search: state.search,
+//   };
+// };
 
-export default connect(mapStateToProps)(Courses);
+// export default connect(mapStateToProps)(Courses);
+export default Courses;
