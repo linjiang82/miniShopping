df6d5d6f3ed22422f6759a2b264888f48255d415
linjiang82 <linjiang82@gmail.com>
2020-04-17 20:37:24 +1000

    implemented main functions

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,8 +1,8 @@
-import React, { RefObject } from 'react'
-import {TCourse} from '../types/types'
-import {Link} from 'react-router-dom'
-import styles from './Course.module.scss'
-import StarRate from './StarRate'
+import React, { RefObject } from "react";
+import { TCourse } from "../types/types";
+import { Link } from "react-router-dom";
+import styles from "./Course.module.scss";
+import StarRate from "./StarRate";
 
 // class Course extends React.Component<{data:TCourse}>{
 //   render(){
@@ -18,22 +18,54 @@ import StarRate from './StarRate'
 //   }
 // }
 
-const Course:React.FC<{data:TCourse;handleQuicklook:(e:React.MouseEvent<HTMLDivElement>)=>void}> = (props)=>{
+const Course: React.FC<{
+  data: TCourse;
+  handleQuicklook: (e: React.MouseEvent<HTMLDivElement>) => void;
+}> = (props) => {
   const quicklookRef = React.useRef<HTMLDivElement>(null);
-    return <div className={styles.outer}>
-    <Link to={`/detail/${props.data.id}`} className={styles.main}>
-    <div className={ styles.course } id = {props.data.id} onMouseEnter={(e)=>{if(e.currentTarget.id===`${props.data.id}`)quicklookRef.current?.classList.remove(styles.showQuicklook);}} onMouseLeave={(e)=>{if(e.currentTarget.id===`${props.data.id}`)quicklookRef.current?.classList.add(styles.showQuicklook);}}>
-      <img className ={ styles.image } src={props.data.thumbURL} alt='thumbnail'></img>
-      <div ref={quicklookRef} id = {props.data.id} className={`${styles.quicklook} ${styles.showQuicklook}`} onClick={(e)=>{e.preventDefault(); props.handleQuicklook(e);}}>Quick Look</div>
-      <div className={styles.price}>$0.00</div>
-      <div className={styles.name}>{props.data.name}</div>
-      <div className={styles.author}>{props.data.author}</div>
-      <div className={styles.rate}>{props.data.rate} 
-      <StarRate rate={props.data.rate}></StarRate>      
-      <span> 12</span></div>
-      </div>
+  return (
+    <div className={styles.outer}>
+      <Link to={`/detail/${props.data.id}`} className={styles.main}>
+        <div
+          className={styles.course}
+          id={props.data.id}
+          onMouseEnter={(e) => {
+            if (e.currentTarget.id === `${props.data.id}`)
+              quicklookRef.current?.classList.remove(styles.showQuicklook);
+          }}
+          onMouseLeave={(e) => {
+            if (e.currentTarget.id === `${props.data.id}`)
+              quicklookRef.current?.classList.add(styles.showQuicklook);
+          }}
+        >
+          <img
+            className={styles.image}
+            src={props.data.thumbURL}
+            alt="thumbnail"
+          ></img>
+          <div
+            ref={quicklookRef}
+            id={props.data.id}
+            className={`${styles.quicklook} ${styles.showQuicklook}`}
+            onClick={(e) => {
+              e.preventDefault();
+              props.handleQuicklook(e);
+            }}
+          >
+            Quick Look
+          </div>
+          <div className={styles.price}>$0.00</div>
+          <div className={styles.name}>{props.data.name}</div>
+          <div className={styles.author}>{props.data.author}</div>
+          <div className={styles.rate}>
+            {props.data.rate}
+            <StarRate rate={props.data.rate}></StarRate>
+            <span> 12</span>
+          </div>
+        </div>
       </Link>
-      </div>
-}
+    </div>
+  );
+};
 
-export default Course
+export default Course;
